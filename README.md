bb1eccfc947e29c81124ac43154e0860be2aec51
Marin <marin@stayke.net>
2014-09-12 20:39:47 -0700

    work for objects and arrays and README

---
+++ README.md
@@ -0,0 +1,19 @@
+jsonfu
+======
+
+Make json pretty.
+
+```sh
+# example with consul
+$ curl http://localhost:8500/v1/kv/somejson -s | jsonfu
+[
+    {
+        "CreateIndex": 16,
+        "Flags": 0,
+        "Key": "somejson",
+        "LockIndex": 0,
+        "ModifyIndex": 19,
+        "Value": "c3R1ZmY="
+    }
+]
+```
--- main.go
+++ main.go
@@ -10,15 +10,28 @@ import (
 
 func main() {
 
+	// either an object or array
+	var out interface{}
+
 	// to json
-	dec := json.NewDecoder(os.Stdin)
-	out := map[string]interface{}{}
-	dec.Decode(&out)
+	decoder := json.NewDecoder(os.Stdin)
+
+	out = []map[string]interface{}{}
+
+	// first try array, if it fails (or empty) try plain obj
+try_again:
+	decoder.Decode(&out)
 
 	pretty, err := json.MarshalIndent(out, "", "\t")
 	if err != nil {
 		fmt.Println("ERROR: ", err)
 		return
+	}
+
+	if len(pretty) == 0 {
+		// didnt work, try plain obj
+		out = map[string]interface{}{}
+		goto try_again
 	}
 
 	os.Stdout.Write(pretty)
